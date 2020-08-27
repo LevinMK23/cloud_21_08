4b64fe470850d2919c3bb85913d6b7acfb33e2e5
Aleksey_Zakharov <a.zakharov@yandex.ru>
2020-08-27 07:45:11 +0300

    HW1-AlekseyZakharov

--- client/src/main/java/Controller.java
+++ client/src/main/java/Controller.java
@@ -7,12 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutionException;
 
@@ -24,7 +22,8 @@ public class Controller implements Initializable {
     private Socket socket;
     private static DataInputStream is;
     private static DataOutputStream os;
-    private String clientPath = "client/ClientStorage";
+
+    private String clientPath = "client/ClientStorage/";
 
     public static void stop() {
         try {
@@ -35,8 +34,62 @@ public class Controller implements Initializable {
         }
     }
 
+
     public void sendMessage(ActionEvent actionEvent) {
         String message = text.getText();
+        if (message.equals("/quit")) {
+            try {
+                os.writeUTF("/quit");
+                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String[] commandMsg = message.split(" ");
+        String command = commandMsg[0];
+        String fileName = commandMsg[1];
+        if (command.equals("/sendFile")) {
+            File file = new File(clientPath + fileName);
+            try (FileInputStream fis = new FileInputStream(file)) {
+                os.writeUTF(command);
+                os.writeUTF(fileName);
+                os.writeLong(file.length());
+                byte[] buf = new byte[256];
+                int read = 0;
+                while ((read = fis.read(buf)) != -1) {
+                    os.write(buf, 0, read);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (command.equals("/downloadFile")) {
+            try {
+                os.writeUTF(command);
+                os.writeUTF(fileName);
+                String answer = is.readUTF();
+                if (answer.equals("OK")) {
+                    long fileLength = is.readLong();
+                    File file = new File(clientPath + fileName);
+                    file.createNewFile();
+                    try (FileOutputStream fos = new FileOutputStream(file)) {
+                        byte[] buf = new byte[256];
+                        if (fileLength < 256) {
+                            fos.write(is.read());
+                        } else {
+                            int read = 0;
+                            for (int i = 0; i < fileLength / 256; i++) {
+                                read = is.read(buf);
+                                fos.write(buf, 0, read);
+                            }
+                        }
+                    }
+
+                } else text.setText("File not found");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
         try {
             os.writeUTF(message);
             os.flush();
@@ -47,9 +100,9 @@ public class Controller implements Initializable {
     }
 
     public void initialize(URL location, ResourceBundle resources) {
-        text.setOnAction(this::sendMessage);
+        text.setOnAction(actionEvent -> sendMessage(actionEvent));
         File dir = new File(clientPath);
-        for (File file : dir.listFiles()) {
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
             listView.getItems().add(file.getName() + "        |       " + file.length() + " bytes");
         }
         try {
@@ -71,19 +124,19 @@ public class Controller implements Initializable {
 //                    }
 //                }
 //            };
-            new Thread(() -> {
-                while (true) {
-                    try {
-                        String message = is.readUTF();
-                        if (message.equals("quit")) {
-                            break;
-                        }
-                        Platform.runLater(() -> listView.getItems().add(message));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
+//            new Thread(() -> {
+//                while (true) {
+//                    try {
+//                        String message = is.readUTF();
+//                        if (message.equals("quit")) {
+//                            break;
+//                        }
+//                        Platform.runLater(() -> listView.getItems().add(message));
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- server/src/main/java/ClientHandler.java
+++ server/src/main/java/ClientHandler.java
@@ -1,7 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
@@ -12,6 +9,7 @@ public class ClientHandler implements Runnable {
     private final Socket socket;
     private static int counter = 0;
     private final String name;
+    private String serverPath = "server/ServerStorage/";
 
     public ClientHandler(Socket socket, IOServer ioServer) throws IOException {
         server = ioServer;
@@ -33,8 +31,6 @@ public class ClientHandler implements Runnable {
         while (true) {
             try {
                 String message = is.readUTF();
-                System.out.println("message from " + name + ": " + message);
-                server.broadCastMessage(message);
                 if (message.equals("quit")) {
                     server.kick(this);
                     os.close();
@@ -42,6 +38,41 @@ public class ClientHandler implements Runnable {
                     socket.close();
                     System.out.println("client " + name + " disconnected");
                     break;
+                }
+                if (message.equals("/sendFile")) {
+                    String fileName = is.readUTF();
+                    long fileLength = is.readLong();
+                    File file = new File(serverPath + fileName);
+                    file.createNewFile();
+                    try (FileOutputStream fos = new FileOutputStream(file)) {
+                        byte[] buf = new byte[256];
+                        if (fileLength < 256) {
+                            fos.write(is.read());
+                        } else {
+                            int read = 0;
+                            for (int i = 0; i < fileLength / 256; i++) {
+                                read = is.read(buf);
+                                fos.write(buf, 0, read);
+                            }
+                        }
+                    }
+                } else {
+                    String fileName = is.readUTF();
+                    File file = new File(serverPath + fileName);
+                    if (file.exists()) {
+                        os.writeUTF("Ok");
+                    }
+                    try (FileInputStream fis = new FileInputStream(file)) {
+                        os.writeUTF(fileName);
+                        os.writeLong(file.length());
+                        byte[] buf = new byte[256];
+                        int read = 0;
+                        while ((read = fis.read(buf)) != -1) {
+                            os.write(buf, 0, read);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
